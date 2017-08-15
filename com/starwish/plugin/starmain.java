83fcb9bd7702480c33e6b7e70aac0db3f8cb0b75
StarWishsama <1552409060@qq.com>
2017-08-15 11:15:33 +0800

    修复配置reload后文件变为0KB (0.1.0 DEV)

--- com/starwish/plugin/starmain.java
+++ com/starwish/plugin/starmain.java
@@ -1,19 +1,15 @@
 package com.starwish.plugin;
 
-import org.bukkit.Bukkit;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import com.starwish.plugin.command.biucmd;
-
 /**
 * 本插件基于 GPL-3.0 开源.
 * 作者 StarWish
 **/
-
 
 public class starmain extends JavaPlugin
 {
@@ -21,19 +17,45 @@ public class starmain extends JavaPlugin
     @Override
 	public void onEnable() 
     {
-		getServer().getConsoleSender().sendMessage("§f[§bStarPlugin§f]欢迎使用 StarPlugin, 作者:StarWish");
-		Bukkit.getPluginCommand("biu").setExecutor(new biucmd());	
+		getServer().getConsoleSender().sendMessage("§bStarPlugin > §f欢迎使用 StarPlugin, 版本" + version +", 作者StarWish");
+		getServer().getConsoleSender().sendMessage("§bStarPlugin > §f感谢您的使用!");
 		saveDefaultConfig();
     }
     
     @Override
     public void onDisable()
     {    	
-    	getServer().getConsoleSender().sendMessage("§f[§bStarPlugin§f]§7正在关闭插件...");
-    	saveConfig();    	
+    	getServer().getConsoleSender().sendMessage("§bStarPlugin > §7正在关闭插件...");  	
     }
-    
-	String prefix = getConfig().getString("config.PluginPrefix");
+
+	String prefix = getConfig().getString("PluginPrefix");
+	String version = getConfig().getString("Version");
+	
+	public boolean onCommand1(CommandSender sender, Command cmd, String label, String[] args) 
+	{
+		if (cmd.getName().equalsIgnoreCase("biu"))
+	    {
+			if (sender.hasPermission("starplugin.biu"))
+			{	
+		        if (!(sender instanceof Player)) 
+		        {
+		        sender.sendMessage("§bStarPlugin > §e你必须在游戏内使用该命令!");
+		        return true;
+		        }
+		    
+		        Player player = (Player) sender;
+	    	  		
+	    	    player.setHealth(0);
+	    	    sender.sendMessage("§bStarPlugin > §e你被苟管理biu了一下!惊喜吧!");
+	    	    return true;
+			}
+			else {
+				sender.sendMessage("§b" + prefix + " §b> §c你没有权限来执行这条命令!");
+			}
+	    }
+		return false;		
+	}
+	
     
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) 
 	{
@@ -41,34 +63,33 @@ public class starmain extends JavaPlugin
 		{			
 		    if (args.length == 0 || sender instanceof ConsoleCommandSender)
 		    {
-			sender.sendMessage("§bStarPlugin V0.0.9 Dev, By StarWish_");
+			sender.sendMessage("§bStarPlugin " + version +", By StarWish_");
 			sender.sendMessage("§f");
 			sender.sendMessage("/biu [玩家] 让一个玩家立即去世");
 			sender.sendMessage("/starp uuid 获取你的UUID");
 			sender.sendMessage("/starp clear 清屏聊天栏");
 			sender.sendMessage("/starp version 显示插件目前版本号");
-			sender.sendMessage("/sptest Debug 命令");
 			sender.sendMessage("/starp reload 重载插件配置");
 			sender.sendMessage("/starp updatelog 查看更新日志");
 			sender.sendMessage("§f");
-			sender.sendMessage("§c§l请注意。本插件不支持/reload命令!");
 			return true;
 		    }
 		    
-		    if (args[0].equalsIgnoreCase("uuid")){
+		    if (args[0].equalsIgnoreCase("uuid"))
+		    {
 		    	
 		    	Player p = (Player) sender;
 		    	
 		    	if (sender.hasPermission("starplugin.uuid"))
 		    	{
 			    	
-			    		sender.sendMessage("§b" + prefix + "§b> §c正在获取您的UUID...");
-				    	sender.sendMessage("§b" + prefix + "§b> §e你的UUID为:" + p.getUniqueId());	
+			    		sender.sendMessage("§b" + prefix + " §b> §c正在获取您的UUID...");
+				    	sender.sendMessage("§b" + prefix + " §b> §e你的UUID为:" + p.getUniqueId());	
 				    	return true;
 			    }
 		    	else 
 		    	{
-		    		sender.sendMessage("§b" + prefix + "§b> §c你没有权限来执行这条命令!");
+		    		sender.sendMessage("§b" + prefix + " §b> §c你没有权限来执行这条命令!");
 		    		return true;
 		    	}	    	
 		    }
@@ -77,20 +98,19 @@ public class starmain extends JavaPlugin
 		    {
 		    	if (sender.hasPermission("starplugin.version") || sender instanceof ConsoleCommandSender)
 		    	{
-					sender.sendMessage("§b" + prefix + "§b> §e目前版本为: §aV0.0.8 DEV");					
+					sender.sendMessage("§b" + prefix + " §b> §e目前版本为: §a" + version);					
 		    	}
 		    	else 
 		    	{
-		    		sender.sendMessage("§b" + prefix + "§b> §c你没有权限来执行这条命令!");
+		    		sender.sendMessage("§b" + prefix + " §b> §c你没有权限来执行这条命令!");
 		    	}
 		      return true;	
 		   }
 		    
 		   if (args[0].equalsIgnoreCase("updatelog") || sender instanceof ConsoleCommandSender)
 		   {
-			   sender.sendMessage("§bV0.0.9 DEV 更新日志:");
-			   sender.sendMessage("将主命令重新返回了 Main.");
-			   sender.sendMessage("新增配置文件, 可以自由配置插件前缀");
+			   sender.sendMessage("§b " + version +"更新日志:");
+			   sender.sendMessage("修复配置.");
 		   }
 		    
 		   if (args[0].equalsIgnoreCase("clear"))
@@ -103,18 +123,26 @@ public class starmain extends JavaPlugin
 			    
 			    if (sender.hasPermission("starplugin.clearscreen"))
 					   {
-				         sender.sendMessage("§b" + prefix + "§b> §c你没有权限来执行这条命令!");
+				         sender.sendMessage("§b" + prefix + " §b> §c你没有权限来执行这条命令!");
 					   } 
 				         for(int i = 0; i <= 50; i++)
 				         sender.sendMessage(" ");
-				         sender.sendMessage("§b" + prefix + "§b> §e清屏成功~"); 
+				         sender.sendMessage("§b" + prefix + " §b> §e清屏成功~"); 
 				         return true;
 		   }
 		   
 		   if (args[0].equalsIgnoreCase("reload"))
 		   {
+			   if (!(sender instanceof ConsoleCommandSender))
+			   {
 			   reloadConfig();
-			   sender.sendMessage("§b" + prefix + "§b> §e重载完成.");
+			   saveConfig();  
+			   sender.sendMessage("§b" + prefix + " §b> §e重载完成.");
+			   }
+			   else
+			   {
+				   sender.sendMessage("§b" + prefix + " §b> §c你没有权限来执行这条命令!");
+			   }
 		   }
 	   }
         return false;
