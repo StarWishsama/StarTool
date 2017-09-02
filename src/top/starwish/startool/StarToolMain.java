aa835c4955bf761c420da61d07e7273c65721c90
StarWishsama <1552409060@qq.com>
2017-09-02 17:14:39 +0800

    新增清屏他人功能 [V0.1.4 Alpha]

--- src/top/starwish/startool/StarToolMain.java
+++ src/top/starwish/startool/StarToolMain.java
@@ -1,14 +1,21 @@
 package top.starwish.startool;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
+
 import top.starwish.startool.command.BiuCommand;
 import top.starwish.startool.command.LabaCommand;
 import top.starwish.startool.listener.*;
+
 
 public class StarToolMain extends JavaPlugin
 {
@@ -19,21 +26,43 @@ public class StarToolMain extends JavaPlugin
     @Override
     public void onEnable()
     {
+    	//加载文件
+    	getServer().getConsoleSender().sendMessage("§b" + prefix + " > 正在载入文件...");
+    	File config = new File(getDataFolder(), "config.yml");
+        FileConfiguration configchange = YamlConfiguration
+                .loadConfiguration(config);
+        if (!config.exists()) 
+        {
+            try 
+            {
+                configchange.set("Version", "0.1.4");
+            	configchange.save(config);
+            } 
+            catch (IOException e) 
+            {
+            	getLogger().warning("在文件加载时出现了错误!");
+                e.printStackTrace();
+            }
+        }
+        saveDefaultConfig();
+        getLogger().info("文件载入完成.");
+        
     	//注册监听器
         getServer().getConsoleSender().sendMessage("§b" + prefix + " > 正在载入监听器...");
         Bukkit.getPluginManager().registerEvents(new LevelChat(), this);
         Bukkit.getPluginManager().registerEvents(new LevelUpTip(), this);
+        getLogger().info("监听器已接入.");
         
         //注册命令
         getServer().getConsoleSender().sendMessage("§b" + prefix + " > 正在注册命令...");
         Bukkit.getPluginCommand("biu").setExecutor(new BiuCommand());
         Bukkit.getPluginCommand("laba").setExecutor(new LabaCommand());
+        getLogger().info("命令已注册.");
         
         //载入完成提示
         getServer().getConsoleSender().sendMessage("§b" + prefix + " > 载入成功.");
         getServer().getConsoleSender().sendMessage("§b" + prefix + " > §f欢迎使用 StarTool, 版本 " + version +", 作者 StarWish");
         getServer().getConsoleSender().sendMessage("§b" + prefix + " > §f感谢您的使用!");
-        saveDefaultConfig();
     }
 
     @Override
@@ -41,6 +70,7 @@ public class StarToolMain extends JavaPlugin
     {
         getServer().getConsoleSender().sendMessage("§b" + prefix + " > §7正在关闭插件...");
     }
+    
 
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
     {
@@ -50,12 +80,12 @@ public class StarToolMain extends JavaPlugin
             {
                 sender.sendMessage("§bStarTool " + version +", By StarWish_");
                 sender.sendMessage("§f");
+                sender.sendMessage("/laba(/lb) [内容] 发送全服公告");
                 sender.sendMessage("/biu <玩家> 让一个玩家立即去世");
                 sender.sendMessage("/stool uuid 获取你的UUID");
-                sender.sendMessage("/stool clear 清屏聊天栏");
-                sender.sendMessage("/stool version 显示插件目前版本号");
+                sender.sendMessage("/stool clear <玩家> 清屏全服/玩家的聊天栏");
+                sender.sendMessage("/stool version 显示插件目前版本");
                 sender.sendMessage("/stool reload 重载插件配置");
-                sender.sendMessage("/stool info 查看部分信息");
                 sender.sendMessage("§f");
                 return true;
             }           
@@ -97,12 +127,30 @@ public class StarToolMain extends JavaPlugin
                 {
                     if (sender.hasPermission("startool.clearscreen"))
                     {
-                        for (int i = 0; i <= 50; i++)
-                        	Bukkit.broadcastMessage("       ");
+                       if (args.length == 1)
+                       {	
+                        for (int i = 0; i <= 60; i++)
+                        Bukkit.broadcastMessage("       ");
                         sender.sendMessage("§b" + prefix + " §b> §e全服清屏成功~");
                         return true;
+                       }
+                       if (sender.hasPermission("startool.clearscreen.other"))
+                       {
+                    	   Player otherp = Bukkit.getPlayer(args[1]);
+                           if (args.length == 2)
+                           {
+                    	    for (int i = 0; i <= 60; i++)
+                    	    otherp.sendMessage("        ");
+                    	    otherp.sendMessage("§b" + prefix + " §b> §e您已被管理员清屏!");
+                    	    sender.sendMessage("§b" + prefix + " §b> §e成功为 §c" + otherp.getName() + " §e清屏!");
+                    	    return true;
+                           }
+                           else if (otherp !=null) {
+                    	   sender.sendMessage("§b" + prefix + " §b> §c玩家名字错误或不存在!");
+                    	   return true;
+                           }
+                       }
                     }
-
                     else
                     {
                         sender.sendMessage("§b" + prefix + " §b> §c你没有权限来执行这条命令!");
@@ -133,7 +181,7 @@ public class StarToolMain extends JavaPlugin
                     return true;
                     } 
             	}   
-            }               	
+            }            
         }
 		return false;
     }
