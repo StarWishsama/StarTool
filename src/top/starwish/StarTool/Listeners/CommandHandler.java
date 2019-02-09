f4a7398a1805a42b77161ad4d1b68ab7fa83682d
StarWishSama <starwishsama@outlook.com>
2019-02-09 16:02:42 +0800

    Update CommandHandler.java

--- src/top/starwish/StarTool/Listeners/CommandHandler.java
+++ src/top/starwish/StarTool/Listeners/CommandHandler.java
@@ -8,7 +8,7 @@ import top.starwish.StarTool.Utils.Utils;
 public class CommandHandler implements Listener {
     @EventHandler
     public void CommandChecker(PlayerCommandPreprocessEvent e) {
-        if (e.getMessage().equalsIgnoreCase("/help startool")) {
+        if (e.getMessage().equalsIgnoreCase("/help startool") || e.getMessage().equalsIgnoreCase("/? startool")) {
             e.getPlayer().sendMessage(Utils.color("&bStarTool > &r请使用 /startool 获取帮助."));
             e.setCancelled(true);
         }
